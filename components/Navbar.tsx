1d45b54a4c819078bbfa86ead568da969488f024
aniketh <anikethbusavale03@gmail.com>
2025-12-14 20:21:18 +0530

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,10 +15,10 @@ export default function Navbar() {
 
                 {/* Center */}
                 <div className="absolute left-1/2 -translate-x-1/2 hidden md:flex gap-8">
-                    <Link href="#" className="hover:opacity-80 transition-opacity">Stays</Link>
-                    <Link href="#" className="hover:opacity-80 transition-opacity">Experiences</Link>
+                    <Link href="/" className="hover:opacity-80 transition-opacity">Stays</Link>
+                    <Link href="/" className="hover:opacity-80 transition-opacity">Experiences</Link>
                     <Link href="/wellness" className="hover:opacity-80 transition-opacity">Wellness</Link>
-                    <Link href="#" className="hover:opacity-80 transition-opacity">Architecture</Link>
+                    <Link href="/" className="hover:opacity-80 transition-opacity">Architecture</Link>
                 </div>
                 {/* Right */}
                 <div className="ml-auto hidden md:block">
