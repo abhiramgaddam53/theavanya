5705a278c552a6ec397acdae5543d3af79fc9d0f
Aniketh <anikethbusavale03@gmail.com>
2025-12-15 15:49:16 +0530

    Revert "first in yash"

--- app/booking/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BookingPage() {
-  return (
-    <div>
-      <h1>Booking</h1>
-      <p>Welcome to our booking system</p>
-    </div>
-  );
-}
--- app/booking/stays-accommodations/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function StaysAccommodationsPage() {
-  return (
-    <div>
-      <h1>Stays & Accommodations</h1>
-      <p>Browse our accommodation options</p>
-    </div>
-  );
-}
